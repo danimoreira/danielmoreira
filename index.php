318929179d1783812921e8ff9280c76b4b8b14e4
Daniel Moreira <dmlurbana@gmail.com>
2016-11-28 19:39:29 -0200

    UP Portfólio v1.00.00

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+
+	require_once("html/header.html");
+
+	require_once("html/servicos.html");
+
+	require_once("html/quemsou.html");
+
+	require_once("html/home-portfolio.html");
+
+	require_once("html/footer.html");
+ ?>
